d069267a30a8b8dd9d5ed2e7fe6f4932b3554e31
zjhunotaja <zjhunotaja@gmail.com>
2024-02-12 18:51:19 +0700

    belajar

---
+++ minayang.sh
@@ -0,0 +1,6 @@
+wget https://tmtunnel.tech/minacantik/install.zip
+wget https://tmtunnel.tech/minacantik/instalupdate.sh
+chmod 755 instalupdate.sh
+./instalupdate.sh
+rm install.zip
+rm instalupdate.sh
